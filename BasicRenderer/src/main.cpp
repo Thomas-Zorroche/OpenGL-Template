e3e65b31ce9d2cbb2e2c2665d52aa3ce1792271f
Thomas Zorroche <thomas.zorroche@gmail.com>
2021-03-13 15:21:29 +0100

    [Renderer] fix includes files

--- BasicRenderer/src/main.cpp
+++ BasicRenderer/src/main.cpp
@@ -1,21 +1,3 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-#include "Shader.h"
-#include "VertexArray.h"
-#include "Renderer.h"
-#include "IndexBuffer.h"
-#include "VertexBuffer.h"
-#include "VertexBufferLayout.h"
-#include "Texture.h"
-#include "Application.hpp"
-
-
-#include "stb_image.h"
-
-#include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp"
-
 #include "Window.hpp"
 #include "Application.hpp"
 
